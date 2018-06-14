102dbff6d8f9cee0d1b938a181497256bd7f9266
Eugene <chepurko0@gmail.com>
2018-06-14 19:23:21 +0300

    Snake
    etc...

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Hello!!! :D");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Hello!!! :D");
+        }
+    }
+}
