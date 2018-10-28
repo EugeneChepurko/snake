dae88f39bd0112a4be9c3aa595585103cb295e45
Eugene <chepurko0@gmail.com>
2018-10-28 22:16:20 +0200

    2_les
    add function Draw();

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,21 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        {          
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Hello!!! :D");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Hello!!! :D");
+        {
+            int x1 = 1;
+            int y1 = 3;
+            char symbol = '*';
+            Draw(x1, y1, symbol);
+
+            int x2 = 2;
+            int y2 = 4;
+            Draw(x2, y2, symbol);
+            Console.WriteLine("dwdw" + x1.GetType());
+        }
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
