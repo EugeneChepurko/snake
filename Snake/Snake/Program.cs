f6aef4957a4c44fef2c88ab7ed148d55e44aa142
Eugene <chepurko0@gmail.com>
2018-11-02 12:21:00 +0200

    5_less_List
    List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,14 +1,26 @@
-﻿namespace Snake
+﻿using System.Collections.Generic;
+
+namespace Snake
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Point p = new Point(1, 3, '*');
-            p.Draw();
+            Point p1 = new Point(1, 3, '*');
+            Point p2 = new Point(2, 4, '*');         
+            Point p3 = new Point(3, 5, '*');         
+            Point p4 = new Point(4, 6, '*');         
 
-            Point p1 = new Point(2, 4, '*');         
-            p1.Draw();
+            List<Point> numList = new List<Point>();
+            numList.Add(p1);
+            numList.Add(p2);
+            numList.Add(p3);
+            numList.Add(p4);
+            
+            foreach (Point x in numList)
+            {
+                x.Draw();
+            }
         }
     }
 }
